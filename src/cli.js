8d6f6e421a9cf6aa85fd264b99583277da4fc922
queckezz <fabian.eichenberger@gmail.com>
2016-11-24 22:29:23 +0100

    improve cli

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,38 @@
 #! /usr/bin/env node
+const { readFileSync } = require('fs')
 const getStdin = require('get-stdin')
 const minimist = require('minimist')
 const { join } = require('path')
 const execa = require('execa')
 
+const help = () => `
+  Usage:
+    inspect-json [file|url]
+  
+  Examples:
+    https://github.com/queckezz/inspect-json#examples
+`
+
 const mainProcess = join(__dirname, 'index.js')
 
-const argv = minimist(process.argv.slice(2))
-const source = argv._[0]
+const args = minimist(process.argv.slice(2), {
+  alias: { h: 'help', v: 'version' }
+})
+
+if (args.help) return console.log(help())
+
+if (args.version) {
+  const contents = readFileSync(join(__dirname, '../package.json'), 'utf-8')
+  return console.log(JSON.parse(contents).version)
+}
+
+const source = args._[0]
 
 if (!source) {
   getStdin().then((str) => {
+    if (str.length == 0) {
+      console.log(help())
+    }
     try {
       const json = JSON.parse(str)
       execa('electron', [mainProcess, JSON.stringify(json)])
